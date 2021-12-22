373d9cfbea5e778acf8fd03c915a82987ad38f8b
Denis Akimov <denis.akimov@gost-group.com>
2021-12-22 13:45:17 +0400

    commit

--- preload.js
+++ preload.js
@@ -1,18 +1,40 @@
 const exec = require('child_process').exec;
+const xlsx = require('xlsx')
+
 
 window.addEventListener('DOMContentLoaded', () => {
-  const input = document.getElementById('input')
+  let path = '';
+  let disabled = true;
+
+  const input = document.querySelector('#field__file-2')
+  const labelForInput = document.querySelector('.field__file-fake')
+  const warning = document.querySelector('.warning')
+  const initialText = labelForInput.innerText
+
   input.addEventListener('change', (event) => {
-    console.log(input.files[0].path)
-    const path = document.getElementById('path')
-    if (input.files[0].path) path.innerText = 'Путь до файла: ' + input.files[0].path
+    if (input?.files?.[0]?.path) {
+      path = input.files[0].path
+      const pathArr = input.files[0].path.split('\\')
+      const fileName = pathArr[pathArr.length - 1]
+      labelForInput.innerText = fileName
+      if (fileName?.split('.')?.[1] === 'xls' || fileName?.split('.')?.[1] === 'xlsx') {
+        disabled = false
+        warning.innerText = ''
+      } else {
+        disabled = true
+        warning.innerText = 'Расширение файла должно быть xls или xlsx'
+      }
+    } else {
+      labelForInput.innerText = initialText
+    }
   })
 
-  const button = document.getElementById('ping')
+  const button = document.getElementById('json')
   button.addEventListener('click', () => {
-    exec('ping yandex.ru', function (error, stdout, stderr) {
-      const other = document.getElementById('other')
-      other.innerText = `${error} - ${stdout} - ${stderr}`
-    })
+    if (!disabled) {
+      const workbook = xlsx.readFile(path, { type: "file" })
+      let sheet_name_list = workbook.SheetNames;
+      console.log(xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+    }
   })
 })
