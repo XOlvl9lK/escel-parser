5c322ed5102db2d71634a565f069d72b5e94eb28
Denis Akimov <denis.akimov@codemark.ru>
2022-03-24 13:42:52 +0400

    Консольное приложение

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,58 @@
-import { app, BrowserWindow } from 'electron'
-import { join } from 'path'
-import { initialize, enable } from '@electron/remote/main'
+import { app } from 'electron'
+import yargs, { Arguments } from "yargs";
+import { existsSync } from "fs";
+import { ValidationService } from "./services/validation.service";
+import { ExcelService } from "./services/excel.service";
+import { LogsPath } from "./services/logger.service";
+import { KafkaService, KafkaSettings } from "./services/kafka.service";
+import { hideBin } from "yargs/helpers";
 
-initialize()
+app.whenReady().then(async () => {
+  console.log('App started')
+  const argv = yargs(hideBin(process.argv)).argv as Arguments<{ path?: string, settings?: string }>
+  const path = argv.path
+  const settings = argv.settings
+  let kafkaSettings: KafkaSettings
+  if (settings && ['test', 'ppak'].includes(settings.toLowerCase())) {
+    console.log(`Creating ${settings} settings`)
+    kafkaSettings = settings.toLowerCase() === 'test' ? KafkaSettings.createTestSettings() : KafkaSettings.createPPAKSettings()
+  } else {
+    console.log(`Creating PPAK settings`)
+    kafkaSettings = KafkaSettings.createPPAKSettings()
+  }
 
-const createWindow = () => {
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: join(__dirname, 'preload.js')
+  if (path) {
+    if (!existsSync(path)) {
+      console.log('File not found')
+      console.log('Exit app')
+      app.quit()
+    } else {
+      console.log('File found')
+      const pathObject = new LogsPath(app.getAppPath())
+      const chunkSize = 4000
+      const validation = new ValidationService(pathObject.getPath())
+      const messagesArr = ExcelService.convertToJSON(path)
+      const validatedRows = messagesArr.map(m => validation.validateRow(m))
+      const rowsForSending = validation.prepareForSending(validatedRows)
+      const kafka = KafkaService.getInstance(
+        pathObject.getPath(),
+        kafkaSettings
+      )
+      for (let i = 0; i < rowsForSending.length; i += chunkSize) {
+        const chunk = rowsForSending.slice(i, i + chunkSize)
+        await kafka.sendMessage('dnPatient', chunk)
+      }
+      console.log('Messages sent')
+      console.log('Logs available at')
+      console.log(pathObject.getPath())
+      console.log('Exit app')
+      app.quit()
     }
-  })
-  enable(window.webContents)
-
-  //@ts-ignore
-  // window.openDevTools();
-
-  window.loadFile(join(__dirname, '../index.html'))
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  console.log(app.getAppPath())
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    console.log(app.getAppPath())
-  })
+  } else {
+    console.log('Path not specified')
+    console.log('Exit app')
+    app.quit()
+  }
 })
 
 app.on('window-all-closed', () => {
